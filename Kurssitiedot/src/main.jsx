74daa7be7f34359459a491c823b544c93f829018
Sannej <siitonenjenna@gmail.com>
2025-03-24 14:00:19 +0200

    Kurssitiedot

---
+++ Kurssitiedot/src/main.jsx
@@ -0,0 +1,5 @@
+import ReactDOM from 'react-dom/client'                     
+
+import App from './App'                                                 // Ladataan App komponentti app.js tiedostosta...
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)    // ...ja renderöidään se root elementtiin/osaan
